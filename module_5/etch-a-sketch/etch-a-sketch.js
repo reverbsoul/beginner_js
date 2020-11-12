7ee13ac2948f12b9afb8620724eb5347712edec8
J Miller <j@successfullysmall.com>
2020-11-12 12:53:00 -0600

    testing etch-a-sketch

--- module_5/etch-a-sketch/etch-a-sketch.js
+++ module_5/etch-a-sketch/etch-a-sketch.js
@@ -1,1 +1,15 @@
 console.log(`running... etch-a-sketch.js`);
+
+const formResponse = document.querySelector('form[name="form"]');
+
+function formAccept(e) {
+  e.preventDefault();
+  console.log('You hit submit');
+  if (!e.currentTarget.agree.checked) {
+    console.log('please agree');
+  } else {
+    console.log('thanks');
+  }
+}
+
+formResponse.addEventListener('submit', formAccept);
